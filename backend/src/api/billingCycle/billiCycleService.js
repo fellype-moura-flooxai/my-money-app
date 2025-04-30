5a26b6391c8425bb28e5beec43016a4682f24873
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-30 13:32:27 -0300

    feat: creating the rest api

---
+++ backend/src/api/billingCycle/billiCycleService.js
@@ -0,0 +1,6 @@
+const BillingCycle = require('./billingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+billingCycle.updateOptions({new: true, runValidators: true})
+
+module.exports = BillingCycle
