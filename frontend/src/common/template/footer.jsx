80716d54d4105f06cd6f6d9dec89a132b11c148a
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-02 12:35:00 -0300

    feat: imported dependencies, created header, menu and footer components

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2025
+            <a href='https://github.com/fellype-moura-flooxai/my-money-app' target='_blank'> Cod3r</a>.
+        </strong>
+    </footer>
+)
