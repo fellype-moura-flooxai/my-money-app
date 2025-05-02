4d0c4fc1d641bcb119c594570f1c36d7cd396e96
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-02 09:31:46 -0300

    feat: testing postman api and getting number of records /count

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,12 +3,12 @@ const BillingCycle = require('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
-BillingCycle.route('get', (req, res, next) => {
-    BillingCycle.find({}, (err, docs) => {
-        if(!err) {
-            res.json(docs)
-        } else {
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
             res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
         }
     })
 })
