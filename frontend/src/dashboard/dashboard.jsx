16284f9a3f5f5f3b391991e9b915d148a3b60048
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-02 13:49:38 -0300

    feat: integrating dashboard with redux and created mapstatetoprops method

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -8,17 +8,18 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
     render() {
+        const { credit, debt } = this.props.summary
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     <Row>
                         <ValueBox cols='12 4' color='green' icon='bank'
-                            value='R$ 10' text='Total de Creditos' />
+                            value={`R$ ${credit}`} text='Total de Creditos' />
                         <ValueBox cols='12 4' color='red' icon='credit-card'
-                            value='R$ 10' text='Total de Debitos' />
+                            value={`R$ ${debt}`} text='Total de Debitos' />
                         <ValueBox cols='12 4' color='blue' icon='money'
-                            value='R$ 0' text='Valor Consolidado' />
+                            value={`R$ ${credit - debt}`} text='Valor Consolidado' />
                     </Row>
                 </Content>
             </div>
@@ -26,5 +27,5 @@ class Dashboard extends Component {
     }
 }
 
-
-export default Dashboard
+const mapStateToProps = state => ({summary: state.dashboard.summary})
+export default connect(mapStateToProps)(Dashboard)
