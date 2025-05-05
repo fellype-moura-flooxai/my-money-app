2aa1944dd27e30b36d93fe2f3e1ec66917ee38b4
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-05 12:09:08 -0300

    feat: display data in billingcyclelist component

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -8,6 +8,17 @@ class BillingCycleList extends Component {
     componentWillMount() {
         this.props.getList()
     }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
     
     render() {
         console.log(this.props.list)
@@ -23,7 +34,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
