ed0603c5d352a9cf2bfab5a9ecd32acec9f1898f
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-05 11:35:16 -0300

    feat: implement the billingcyclelist component to list the billing cycles

---
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+
+class BillingCycleList extends Component {
+
+    render() {
+        return (
+            <div>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Mês</th>
+                            <th>Ano</th>
+                            <th className='table-actions'>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default BillingCycleList
+
