66f1ed5b51dbf81cf94bf095ee689d4fd055d260
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-06 11:41:15 -0300

    feat: creditList component, Input for creditlist

---
+++ frontend/src/billingCycle/creditList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { Field } from 'redux-form'
+import Grid from '../common/layout/grid'
+import Input from '../common/form/input'
+
+class CreditList extends Component {
+
+    renderRows() {
+        return (
+            <tr>
+                <td><Field name='credits[0].name' component={Input}
+                    placeholder='informe o nome' readOnly={this.props.readOnly} /></td>
+                <td><Field name='credits[0].value' component={Input}
+                    placeholder='informe o valor' readOnly={this.props.readOnly} /></td>
+            </tr>
+        )
+    }
+
+    render() {
+        return (
+            <Grid cols={this.props.cols}>
+                <fieldset>
+                    <legend>creditos</legend>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Valor</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.renderRows()}
+                        </tbody>
+                    </table>
+                </fieldset>
+            </Grid>
+        )
+    }
+}
+
+export default CreditList
